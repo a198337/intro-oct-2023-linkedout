c6179402674fe2dc6b1ea9481787d8af785e58c0
helen_kim@progressive.com <helen_kim@progressive.com>
2023-10-30 17:24:55 -0400

    removed the edits to make vscode custom colored around the borders and other parts of it

--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: ["light", "dark", "forest"],
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
