b49ddcc1f44122c81120bbd8dc580fd465f3dcbe
helen_kim@progressive.com <helen_kim@progressive.com>
2023-10-30 15:15:43 -0400

    basic angular before last break

---
+++ frontend/frontend/src/app/components/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template: `
+    <header>
+    <p>Linked Out</p>
+      <p>Our Application</p>
+    </header>
+    `,
+    standalone: true
+})
+export class HeaderComponent {
+
+}
